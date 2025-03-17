7c3020a8b93cc06f2c4917c4d1d804ed33c4ba80
fxwan <fxwan@stu.ecnu.edu.cn>
2025-03-17 14:37:47 +0800

    Clean_data
    Drop unnecessary columns

--- Comparison_1/step1_spinup_information.R
+++ Comparison_1/step1_spinup_information.R
@@ -1,71 +1,79 @@
-setwd('/Users/wan/1-WanWork/Case1-TraceE/9-TECOCNP图文修改/NewSpinup20250104/tt3/teco_cnptt13/')
+# Fangxiu Wan - last update 20250317
+
+# This code is for saving current values for the next timestep
+# Some variables need to be preserved for the next simulation step
+
+# Data for figures 4, 5
+
+setwd("your_path")
+
 library(dplyr)
 library(tidyr)
 
-QC<-read.csv('teco_8cpools_cnp.csv',header = FALSE)
-CN<-read.csv('CNP_Ncycle_simu_ratios_cnp.csv',header = FALSE)
-CP<-read.csv('CNP_Pcycle_simu_ratios_cnp.csv',header = FALSE)
+QC <- read.csv("teco_Cpools_cnp.csv", header = FALSE)
+CN <- read.csv("CNP_Ncycle_simu_ratios_cnp.csv", header = FALSE)
+CP <- read.csv("CNP_Pcycle_simu_ratios_cnp.csv", header = FALSE)
 
-QC0<-colMeans(QC)
-CN0<-colMeans(CN)
-CP0<-colMeans(CP)
+QC0 <- colMeans(QC)
+CN0 <- colMeans(CN)
+CP0 <- colMeans(CP)
 
-Initialstate<-round(as.data.frame((rbind(QC0,CN0,CP0))),digits = 1)
-write.csv(Initialstate,'A_Initialstate_fromspinup_cnp.csv',row.names = FALSE)
-
-
-c_end<-read.csv('teco_simu_cflux_cnp.csv',header = FALSE)
-c_need<-c_end[,c(4,6,24)]
-
-n_end<-read.csv('CNP_NNNcycle_dynamics_lastyear_cnp.csv',header = FALSE)
-n_need<-n_end[,9]
-
-p_end<-read.csv('CNP_PPPcycle_dynamics_lastyear_cnp.csv',header = FALSE)
-
-p_need<-p_end[,c(10:12)]
+Initialstate <- round(as.data.frame((rbind(QC0, CN0, CP0))), digits = 1)
+write.csv(Initialstate, "A_Initialstate_fromspinup_cnp.csv", row.names = FALSE)
 
 
-need<-cbind(c_need,n_need,p_need)
-colnames(need)<-c('LAI','NSC','accumulation','QNminer','QPlab',	'QPsorb',	'QPss')
-write.csv(need[365,],'A_need_fromlastyearlastday_cnp.csv')
+c_end <- read.csv("teco_simu_cflux_cnp.csv", header = FALSE)
+c_need <- c_end[, c(4, 5, 9)] 
+
+n_end <- read.csv("CNP_NNNcycle_dynamics_lastyear_cnp.csv", header = FALSE)
+n_need <- n_end[, 9]
+
+p_end <- read.csv("CNP_PPPcycle_dynamics_lastyear_cnp.csv", header = FALSE)
+p_need <- p_end[, c(7:9)] 
+
+
+need <- cbind(c_need, n_need, p_need)
+colnames(need) <- c("LAI", "NSC", "accumulation", "QNminer", "QPlab", "QPsorb", "QPss")
+write.csv(need[365, ], "A_need_fromlastyearlastday_cnp.csv")
 
 
 ##### CN #####
-setwd('/Users/wan/1-WanWork/Case1-TraceE/9-TECOCNP图文修改/NewSpinup20250104/tt3/teco_cntt13/')
-QC<-read.csv('teco_8cpools_cn.csv',header = FALSE)
-CN<-read.csv('CNP_Ncycle_simu_ratios_cn.csv',header = FALSE)
+setwd("your_path")
 
-QC0<-colMeans(QC)
-CN0<-colMeans(CN)
+QC <- read.csv("teco_Cpools_cn.csv", header = FALSE)
+CN <- read.csv("CNP_Ncycle_simu_ratios_cn.csv", header = FALSE)
 
-Initialstate<-round(as.data.frame((rbind(QC0,CN0))),digits = 1)
-write.csv(Initialstate,'A_Initialstate_fromspinup_cn.csv',row.names = FALSE)
+QC0 <- colMeans(QC)
+CN0 <- colMeans(CN)
+
+Initialstate <- round(as.data.frame((rbind(QC0, CN0))), digits = 1)
+write.csv(Initialstate, "A_Initialstate_fromspinup_cn.csv", row.names = FALSE)
 
 
-c_end<-read.csv('teco_simu_cflux_cn.csv',header = FALSE)
-c_need<-c_end[,c(4,6,24)]
+c_end <- read.csv("teco_simu_cflux_cn.csv", header = FALSE)
+c_need <- c_end[, c(4, 5, 9)] # 4,5,9 for clean data
 
-n_end<-read.csv('CNP_NNNcycle_dynamics_lastyear_cn.csv',header = FALSE)
-n_need<-n_end[,9]
+n_end <- read.csv("CNP_NNNcycle_dynamics_lastyear_cn.csv", header = FALSE)
+n_need <- n_end[, 9]
 
-need<-cbind(c_need,n_need)
-colnames(need)<-c('LAI','NSC','accumulation','QNminer')
-write.csv(need[365,],'A_need_fromlastyearlastday_cn.csv')
+need <- cbind(c_need, n_need)
+colnames(need) <- c("LAI", "NSC", "accumulation", "QNminer")
+write.csv(need[365, ], "A_need_fromlastyearlastday_cn.csv")
 
 ##### C #####
-setwd('/Users/wan/1-WanWork/Case1-TraceE/9-TECOCNP图文修改/NewSpinup20250104/tt3/teco_ctt13/')
+setwd("your_path")
 
-QC<-read.csv('teco_8cpools_c.csv',header = FALSE)
+QC <- read.csv("teco_Cpools_c.csv", header = FALSE)
 
-QC0<-colMeans(QC)
+QC0 <- colMeans(QC)
 
-Initialstate<-round(as.data.frame((rbind(QC0))),digits = 1)
-write.csv(Initialstate,'A_Initialstate_fromspinup_c.csv',row.names = FALSE)
+Initialstate <- round(as.data.frame((rbind(QC0))), digits = 1)
+write.csv(Initialstate, "A_Initialstate_fromspinup_c.csv", row.names = FALSE)
 
 
-c_end<-read.csv('teco_simu_cflux_c.csv',header = FALSE)
-c_need<-c_end[,c(4,6,24)]
+c_end <- read.csv("teco_simu_cflux_c.csv", header = FALSE)
+c_need <- c_end[, c(4, 5, 9)] 
 
-need=c_need
-colnames(need)<-c('LAI','NSC','accumulation')
-write.csv(need[365,],'A_need_fromlastyearlastday_c.csv')
+need <- c_need
+colnames(need) <- c("LAI", "NSC", "accumulation")
+write.csv(need[365, ], "A_need_fromlastyearlastday_c.csv")
--- Comparison_1/step2_transient_information.R
+++ Comparison_1/step2_transient_information.R
@@ -1,126 +1,130 @@
+# Fangxiu Wan - last update 20250317
+
+# This code is for saving current values from transient run
+
+# Data for figures 4, 5
 
 library(dplyr)
 library(tidyr)
 
 # C pools, Cflux
 
-setwd('/Users/wan/1-WanWork/Case1-TraceE/9-TECOCNP图文修改/NewSpinup20250104/tt13/TR/teco_cnp')
+setwd("your_path")
 
-QC<-read.csv('simu_8cpools_lastyear_cnp.csv',header = FALSE)
-reQC<-colMeans(QC)
+QC <- read.csv("simu_Cpools_lastyear_cnp.csv", header = FALSE)
+reQC <- colMeans(QC)
 reQC
-outQC<-read.csv('teco_outc_cnp.csv',header = FALSE)
-reoutQC<-colSums(outQC[c(6941:7305),])
+outQC <- read.csv("teco_outc_cnp.csv", header = FALSE)
+reoutQC <- colSums(outQC[c(6941:7305), ])
 reoutQC
-QC_data<-round(as.data.frame(rbind(reQC,reoutQC)),digit=1)
-write.csv(QC_data,'A_QC_data_cnp.csv')
-cflux<-read.csv('simu_cflux_lastyear_record_cnp.csv',header = FALSE)
-recflux<-cflux[,c(1:4,8)] # GPP,ER,NEE,LAI,Rh
+QC_data <- round(as.data.frame(rbind(reQC, reoutQC)), digit = 1)
+write.csv(QC_data, "A_QC_data_cnp.csv")
+cflux <- read.csv("simu_cflux_lastyear_record_cnp.csv", header = FALSE)
+recflux <- cflux[, c(1:4, 8)] # GPP,ER,NEE,LAI,Rh. # clean data - 1:5
 colSums(recflux)
-mean(recflux[,5]) #Rh
-sd(recflux[,5]) #Rh
-mean(recflux[,4]) #LAI
-write.csv(recflux,'A_result_cflux_cnp.csv')
+mean(recflux[, 5]) # Rh
+sd(recflux[, 5]) # Rh
+mean(recflux[, 4]) # LAI
+write.csv(recflux, "A_result_cflux_cnp.csv")
 
-# - 
+# -
 
 
 ## N cycle ##
-QN<-read.csv('CNP_Ncycle_simu_pools_lastyear_cnp.csv',header = FALSE)
-QN[365,]
-reQN<-colMeans(QN)
+QN <- read.csv("CNP_Ncycle_simu_pools_lastyear_cnp.csv", header = FALSE)
+QN[365, ]
+reQN <- colMeans(QN)
 reQN
-outQN<-read.csv('teco_outn_cnp.csv',header = FALSE)
-reoutQN<-colSums(outQN[c(6941:7305),])
+outQN <- read.csv("teco_outn_cnp.csv", header = FALSE)
+reoutQN <- colSums(outQN[c(6941:7305), ])
 reoutQN
 
-QN_data<-round(as.data.frame(rbind(reQN,reoutQN)),digit=1)
+QN_data <- round(as.data.frame(rbind(reQN, reoutQN)), digit = 1)
 QN_data
-write.csv(QN_data,'A_QN_data_cnp.csv')
+write.csv(QN_data, "A_QN_data_cnp.csv")
 
-QNminer<-read.csv('CNP_NNNcycle_dynamics_lastyear_cnp.csv',header = FALSE)
-Nmin<-sum(QNminer[,5])
-QNmin<-mean(QNminer[,9])
+QNminer <- read.csv("CNP_NNNcycle_dynamics_lastyear_cnp.csv", header = FALSE)
+Nmin <- sum(QNminer[, 5])
+QNmin <- mean(QNminer[, 9])
 
-MinerN<-round(cbind(Nmin,QNmin),digits = 1)
+MinerN <- round(cbind(Nmin, QNmin), digits = 1)
 MinerN
-write.csv(MinerN,'A_minerN_flux_cnp.csv')
+write.csv(MinerN, "A_minerN_flux_cnp.csv")
 
 
 ## P cycle ##
-QP<-read.csv('CNP_Pcycle_simu_pools_lastyear_cnp.csv',header = FALSE)
-reQP<-colMeans(QP)
+QP <- read.csv("CNP_Pcycle_simu_pools_lastyear_cnp.csv", header = FALSE)
+reQP <- colMeans(QP)
 reQP
-outQP<-read.csv('teco_outp_cnp.csv',header = FALSE)
-write.csv(as.data.frame(outQP[c(6941:7305),]),'last_outP_cnp_test.csv')
-reoutQP<-colSums(outQP[c(6941:7305),])
+outQP <- read.csv("teco_outp_cnp.csv", header = FALSE)
+write.csv(as.data.frame(outQP[c(6941:7305), ]), "last_outP_cnp_test.csv")
+reoutQP <- colSums(outQP[c(6941:7305), ])
 reoutQP
 
-QP_data<-round(as.data.frame(rbind(reQP,reoutQP)),digit=1)
-write.csv(QP_data,'A_QP_data_cnp.csv')
+QP_data <- round(as.data.frame(rbind(reQP, reoutQP)), digit = 1)
+write.csv(QP_data, "A_QP_data_cnp.csv")
 
-QPlab<-read.csv('CNP_PPPcycle_dynamics_lastyear_cnp.csv',header = FALSE)
-Pmin<-sum(QPlab[,2])
-QPlab_size<-mean(QPlab[,10])
+QPlab <- read.csv("CNP_PPPcycle_dynamics_lastyear_cnp.csv", header = FALSE)
+Pmin <- sum(QPlab[, 2])
+QPlab_size <- mean(QPlab[, 10])
 
-MinerP<-round(cbind(Pmin,QPlab_size),digits = 1)
+MinerP <- round(cbind(Pmin, QPlab_size), digits = 1)
 MinerP
-write.csv(MinerP,'A_minerP_flux_cnp.csv')
+write.csv(MinerP, "A_minerP_flux_cnp.csv")
 
-rm(list=ls())
+rm(list = ls())
 
 ##### CN #####
-setwd('/Users/wan/1-WanWork/Case1-TraceE/9-TECOCNP图文修改/NewSpinup20250104/tt13/TR/teco_cn')
-QC<-read.csv('simu_8cpools_lastyear_cn.csv',header = FALSE)
-reQC<-colMeans(QC)
+setwd("your_path")
+QC <- read.csv("simu_Cpools_lastyear_cn.csv", header = FALSE)
+reQC <- colMeans(QC)
 reQC
-outQC<-read.csv('teco_outc_cn.csv',header = FALSE)
-reoutQC<-colSums(outQC[c(6941:7305),])
+outQC <- read.csv("teco_outc_cn.csv", header = FALSE)
+reoutQC <- colSums(outQC[c(6941:7305), ])
 reoutQC
-QC_data<-round(as.data.frame(rbind(reQC,reoutQC)),digit=1)
-write.csv(QC_data,'A_QC_data_cn.csv')
-cflux<-read.csv('simu_cflux_lastyear_record_cn.csv',header = FALSE)
-recflux<-cflux[,c(1:4,8)]
+QC_data <- round(as.data.frame(rbind(reQC, reoutQC)), digit = 1)
+write.csv(QC_data, "A_QC_data_cn.csv")
+cflux <- read.csv("simu_cflux_lastyear_record_cn.csv", header = FALSE)
+recflux <- cflux[, c(1:4, 8)]
 colSums(recflux)
-LAI<-cflux[,4]
-mean(recflux[,4]) #LAI
-write.csv(recflux,'A_result_cflux_cn.csv')
+LAI <- cflux[, 4]
+mean(recflux[, 4]) # LAI
+write.csv(recflux, "A_result_cflux_cn.csv")
 
 ## N cycle ##
-QN<-read.csv('CNP_Ncycle_simu_pools_lastyear_cn.csv',header = FALSE)
-reQN<-colMeans(QN)
+QN <- read.csv("CNP_Ncycle_simu_pools_lastyear_cn.csv", header = FALSE)
+reQN <- colMeans(QN)
 reQN
-outQN<-read.csv('teco_outn_cn.csv',header = FALSE)
-reoutQN<-colSums(outQN[c(6941:7305),])
+outQN <- read.csv("teco_outn_cn.csv", header = FALSE)
+reoutQN <- colSums(outQN[c(6941:7305), ])
 reoutQN
 
-QN_data<-round(as.data.frame(rbind(reQN,reoutQN)),digit=1)
-write.csv(QN_data,'A_QN_data_cn.csv')
+QN_data <- round(as.data.frame(rbind(reQN, reoutQN)), digit = 1)
+write.csv(QN_data, "A_QN_data_cn.csv")
 
-QNminer<-read.csv('CNP_NNNcycle_dynamics_lastyear_cn.csv',header = FALSE)
-Nmin<-sum(QNminer[,5])
-QNmin<-mean(QNminer[,9])
+QNminer <- read.csv("CNP_NNNcycle_dynamics_lastyear_cn.csv", header = FALSE)
+Nmin <- sum(QNminer[, 5])
+QNmin <- mean(QNminer[, 9])
 
-MinerN<-round(cbind(Nmin,QNmin),digits = 1)
-write.csv(MinerN,'A_minerN_flux_cn.csv')
+MinerN <- round(cbind(Nmin, QNmin), digits = 1)
+write.csv(MinerN, "A_minerN_flux_cn.csv")
 
-rm(list=ls())
+
+rm(list = ls())
 ##### C #####
-setwd('/Users/wan/1-WanWork/Case1-TraceE/9-TECOCNP图文修改/NewSpinup20250104/tt13/TR/teco_c')
-
-QC<-read.csv('simu_8cpools_lastyear_c.csv',header = FALSE)
-reQC<-colMeans(QC)
+setwd("your_path")
+QC <- read.csv("simu_Cpools_lastyear_c.csv", header = FALSE)
+reQC <- colMeans(QC)
 reQC
-outQC<-read.csv('teco_outc_c.csv',header = FALSE)
-reoutQC<-colSums(outQC[c(6941:7305),])
+outQC <- read.csv("teco_outc_c.csv", header = FALSE)
+reoutQC <- colSums(outQC[c(6941:7305), ])
 reoutQC
-QC_data<-round(as.data.frame(rbind(reQC,reoutQC)),digit=1)
-write.csv(QC_data,'A_QC_data_c.csv')
-cflux<-read.csv('simu_cflux_lastyear_record_c.csv',header = FALSE)
-recflux<-cflux[,c(1:4,8)]
+QC_data <- round(as.data.frame(rbind(reQC, reoutQC)), digit = 1)
+write.csv(QC_data, "A_QC_data_c.csv")
+cflux <- read.csv("simu_cflux_lastyear_record_c.csv", header = FALSE)
+recflux <- cflux[, c(1:4, 8)]
 colSums(recflux)
-colSums(recflux)/8760
-LAI<-mean(cflux[,4])
+colSums(recflux) / 8760
+LAI <- mean(cflux[, 4])
 LAI
-write.csv(recflux,'A_result_cflux_c.csv')
-
+write.csv(recflux, "A_result_cflux_c.csv")
